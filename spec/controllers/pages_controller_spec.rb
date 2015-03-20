c9109e468487f40f35728b0c9bc2cb2c88e3da2d
Pallas <pallasforcella@teletech.com>
2015-03-20 09:11:36 -0600

    commiting

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,54 +5,39 @@ RSpec.describe PagesController, type: :controller do
   describe PagesController do
     render_views
 
-  describe "GET #home" do
-    expect(page).to have_title "Home"
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
+    describe "GET #home" do
+      
+      it "returns http success" do
+        get :home
+        expect(response).to have_http_status(:success)
+      end
+
+      it "should have the right title" do
+        visit "/pages/home"
+        expect(page).to have_title "Home"
+      end
     end
 
-    it "should have the right title" do
-      get :home
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Home"
-  end
-end
-end
-
-  describe "GET #contact" do
-    expect(page).to have_title "Contact"
-    it "returns http success" do
-      get :contact
-      expect(response).to have_http_status(:success)
+    describe "GET #contact" do
+      it "returns http success" do
+        get :contact
+        expect(response).to have_http_status(:success)
+      end
+       it "should have the right title" do
+        visit "/pages/contact"
+        expect(page).to have_title "Contact"
+     end
+   end
+    describe "GET #about" do
+      it "returns http success" do
+        get :about
+        expect(response).to have_http_status(:success)
+      end
+       it "should have the right title" do
+        visit "/pages/about"
+        expect(page).to have_title "About"
+      end
     end
-     it "should have the right title" do
-      get :contact
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Contact"
   end
 end
-end
-   describe "GET #about" do
-    expect(page).to have_title "About"
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
-     it "should have the right title" do
-      get :about
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | About"
-  end
-end
-end
-class PagesController < ApplicationController
 
-  def home
-    @title = "Home"
-  end
-
-  def contact
-    @title = "Contact"
-  end
-
-  def about
-    @title = "About"
-  end
